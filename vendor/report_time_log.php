18bd3ed2f4aa2caccbdd2c05287f8ac082b6db48
tomoe1337 <igrikbin@gmail.com>
2025-01-02 18:23:15 +0300

    order by date desc

--- vendor/report_time_log.php
+++ vendor/report_time_log.php
@@ -6,7 +6,8 @@
 	$sql = "SELECT t.date, u.full_name, t.arrived, t.left 
 	FROM time_log t 
 	LEFT JOIN users u on t.manager_id = u.id
-	WHERE date >= '$date'";
+	WHERE date >= '$date'
+	ORDER BY `t`.`date` DESC";
 
 	$month_time_log= mysqli_query($connect,$sql);
 
