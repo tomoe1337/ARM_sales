890e8c667131f76aa169e524c5aac3aea7594ff5
Nazar Khodzhaev <hodzhaev.nazar@proredcat.com>
2025-05-10 13:22:05 +0300

    fix some code

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('/', function () {
         Route::get('/clients', ClientIndexController::class)->name('clients.index');
         Route::get('/clients/create', ClientCreateController::class)->name('clients.create');
         Route::post('/clients', ClientStoreController::class)->name('clients.store');
-        //Route::get('/clients/{client}', ClientShowController::class)->name('clients.index');
+        Route::get('/clients/{client}', ClientShowController::class)->name('clients.show');
         Route::get('/clients/{client}/edit', ClientEditController::class)->name('clients.edit');
         Route::delete('/clients/{client}', ClientDestroyController::class)->name('clients.destroy');
 
@@ -82,7 +82,7 @@ Route::get('/', function () {
         Route::get('/deals', DealIndexController::class)->name('deals.index');
         Route::get('/deals/create', DealCreateController::class)->name('deals.create');
         Route::post('/deals', DealStoreController::class)->name('deals.store');
-        //Route::get('/deals/{deal}', DealShowController::class)->name('deals.index');
+        Route::get('/deals/{deal}', DealShowController::class)->name('deals.show');
         Route::get('/deals/report/day', DealReportDayController::class)->name('deals.report.day');
         Route::delete('/deals/{deal}', \App\Http\Controllers\Deal\DestroyController::class)->name('deals.destroy');
 
