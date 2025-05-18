72d7fbb391d49867e0acb312f03a1da854cd1b2b
igrikbin <igrikbin@gmail.com>
2025-05-18 13:36:10 +0000

    fix some sode

--- resources/views/work-sessions/report.blade.php
+++ resources/views/work-sessions/report.blade.php
@@ -63,6 +63,7 @@
                                                 </td>
                                             </tr>
                                         @endforeach
+    @endif
                             </tbody>
                         </table>
                     </div>
